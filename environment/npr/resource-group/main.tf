fff9ae69c37a0201f63f5fdbc961a26b8a41150f
chut-th <chutchawin.th@outlook.com>
2024-09-21 11:43:59 +0700

    Update naming of the resource group module and add output file

--- environment/npr/resource-group/main.tf
+++ environment/npr/resource-group/main.tf
@@ -1,4 +1,4 @@
-module "resource-group-nw-01" {
+module "resource_group_nw_01" {
   source  = "chut-th/resource-group/azurerm"
   version = "1.0.1"
   company = "ct"
@@ -8,7 +8,7 @@ module "resource-group-nw-01" {
   running_number = "01"
 }
 
-module "resource-group-sta-01" {
+module "resource_group_sta_01" {
   source  = "chut-th/resource-group/azurerm"
   version = "1.0.1"
   company = "ct"
---
+++ environment/npr/resource-group/output.tf
@@ -0,0 +1,19 @@
+output "resource_group_nw_01_name" {
+  description = "Resource group name"
+  value = module.resource_group_nw_01.name
+}
+
+output "resource_group_nw_01_id" {
+  description = "Resource group ID"
+  value = module.resource_group_nw_01.ID
+}
+
+output "resource_group_sta_01_name" {
+  description = "Resource group name"
+  value = module.resource_group_sta_01.name
+}
+
+output "resource_group_sta_01_id" {
+  description = "Resource group ID"
+  value = module.resource_group_sta_01.ID
+}
